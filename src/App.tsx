7196bffe8b68cb167708cb2c4f30d939d3830a48
Seishiro Takamatsu <takataka4072715@gmail.com>
2022-04-17 14:06:40 +0900

    Add example code

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,103 @@
-import React from 'react';
-import logo from './logo.svg';
+import {
+  CompleteNewPasswordRoute,
+  PrivateRoute,
+  SignInRoute,
+  useAuthStatus,
+  useSignIn,
+  useSignOut,
+} from 'cogreact';
+import React, { useState } from 'react';
+import { Link, Route, Routes, useLocation } from 'react-router-dom';
 import './App.css';
 
 function App() {
+  const authState = useAuthStatus();
+  const location = useLocation();
+
+  const { signIn, loading, error } = useSignIn();
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleClick = () => {
+    signIn({ email, password });
+  };
+
+  const { signOut } = useSignOut();
+
+  const handleClickSignOut = () => {
+    signOut();
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Routes</h1>
+      <p>AuthStatus: {authState.authStatus}</p>
+      <p>Pathname: {location.pathname}</p>
+      <p>
+        Location State: {(location.state as { from?: string })?.from as string}
+      </p>
+
+      <ul>
+        <li>
+          <Link to="/">Index</Link>
+        </li>
+        <li>
+          <Link to="private1">Private 1</Link>
+        </li>
+        <li>
+          <Link to="private2">Private 2</Link>
+        </li>
+        <li>
+          <Link to="login">Login</Link>
+        </li>
+        <li>
+          <Link to="login">Complete New Password</Link>
+        </li>
+        <li>
+          <button onClick={handleClickSignOut}>Sign out</button>
+        </li>
+      </ul>
+
+      <div>
+        <h2>Sign in</h2>
+
+        <div>
+          <input
+            type="text"
+            name="email"
+            placeholder="email"
+            value={email}
+            onChange={(e) => setEmail(e.currentTarget.value)}
+          />
+          <input
+            type="text"
+            placeholder="password"
+            value={password}
+            onChange={(e) => setPassword(e.currentTarget.value)}
+          />
+          <button onClick={handleClick}>Sign In</button>
+        </div>
+      </div>
+
+      <Routes>
+        <Route element={<PrivateRoute />}>
+          <Route path="/" element={<>Index</>} />
+          <Route path="private1" element={<>Private 1</>} />
+          <Route path="private2" element={<>Private 2</>} />
+        </Route>
+
+        <Route element={<SignInRoute />}>
+          <Route path="login" element={<>Login</>} />
+        </Route>
+
+        <Route element={<CompleteNewPasswordRoute />}>
+          <Route
+            path="complete-new-password"
+            element={<>Complete New Password</>}
+          />
+        </Route>
+      </Routes>
     </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,41 @@
+import { Cogreact } from 'cogreact';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import { BrowserRouter } from 'react-router-dom';
+import { RecoilRoot } from 'recoil';
 import App from './App';
+import './index.css';
 import reportWebVitals from './reportWebVitals';
+
+const region = process.env.REACT_APP_REGION;
+const identityPoolId = process.env.REACT_APP_IDENTITY_POOL_ID;
+const userPoolId = process.env.REACT_APP_USER_POOL_ID;
+const userPoolWebClientId = process.env.REACT_APP_USER_POOL_WEB_CLIENT_ID;
+
+if (!region || !identityPoolId || !userPoolId || !userPoolWebClientId) {
+  throw new Error('Please set environment variables.');
+}
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <RecoilRoot>
+        <Cogreact
+          AuthConfig={{
+            region,
+            identityPoolId,
+            userPoolId,
+            userPoolWebClientId,
+          }}
+          loadingComponent={<p>ローディング中です</p>}
+        >
+          <App />
+        </Cogreact>
+      </RecoilRoot>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
